77fefad79f396eef8ee4d9cb70d08737564965a1
Alexander Klink <git@alech.de>
2012-07-29 14:24:44 +0200

    send to fallblatt and reply to tweet

--- fallblatt_tw.hs
+++ fallblatt_tw.hs
@@ -15,9 +15,12 @@ import Network.OAuth.Http.CurlHttpClient
 
 import Network.OAuth.Http.Request
 import Network.OAuth.Http.Response
+import qualified Network.OAuth.Http.PercentEncoding as PE
 
 import Text.JSON
 import Data.Ratio
+import Text.Printf
+import Network.Socket
 
 data TwitterBotConfig = TwitterBotConfig { 
 		consumerKey       :: String,
@@ -28,8 +31,12 @@ data TwitterBotConfig = TwitterBotConfig {
 	deriving (Eq, Show)
 
 mentionsUrl params = fromJust $ parseURL $ "https://api.twitter.com/1/statuses/mentions.json" ++ params
+tweetUrl = fromJust $ parseURL "https://api.twitter.com/1/statuses/update.json"
 
-sleepTime = 15 * 10^6 -- 15 seconds
+sleepTime     = 15 * 10^6 -- 15 seconds
+maxSize       = 15 -- Fallblattanzeige has 15 chars
+fallblattHost = "127.0.0.1"
+fallblattPort = 1337
 
 main = do
 	-- parse command line arguments
@@ -44,29 +51,86 @@ main = do
 
 mainLoop :: TwitterBotConfig -> Maybe Integer -> IO ()
 mainLoop twitterBotConfig lastId = do
-	response <- getMentions twitterBotConfig lastId
-	putStrLn $ show response
+	mentionsResponse <- getMentions twitterBotConfig lastId
+	let currentId  = extractLastId mentionsResponse
+	let mention    = extractMention mentionsResponse
+	let screenName = extractScreenName mentionsResponse
+	fallblattResponse <- maybeShowOnFallblatt mention screenName
+	maybeTweetReply lastId twitterBotConfig screenName currentId fallblattResponse
 	threadDelay sleepTime
-	let newLid = extractLastId response
-	mainLoop twitterBotConfig (extractLastId response)
+	mainLoop twitterBotConfig (nothingOr currentId lastId)
+
+maybeShowOnFallblatt :: Maybe String -> Maybe String -> IO (Maybe String)
+maybeShowOnFallblatt (Just mention) maybeScreenName = do
+	if length mention > maxSize then
+		return (Just $ "TMI. Please limit yourself to " ++ (show maxSize) ++ " characters!")
+	else
+		setFallblatt $ maybeCombine mention maybeScreenName
+maybeShowOnFallblatt _ _ = do
+	return Nothing
+
+-- if there is enough space, combine screen name and mention, i.e.
+-- "alech: foobar"
+maybeCombine :: String -> Maybe String -> String
+maybeCombine mention (Just screenName) =
+	head $ filter (\e -> length e <= maxSize) [combined, mention]
+	where combined = screenName ++ ": " ++ mention
+maybeCombine mention Nothing = mention
+
+setFallblatt :: String -> IO (Maybe String)
+setFallblatt text = withSocketsDo $ do
+	s <- socket AF_INET Datagram defaultProtocol
+	hostAddr <- inet_addr fallblattHost
+	sendTo s (printf ("%-" ++ (show maxSize) ++ "s\n") text) (SockAddrInet fallblattPort hostAddr)
+	response <- recv s 16
+	sClose s
+	return (Just response)
+
+maybeTweetReply :: (Maybe Integer) -> TwitterBotConfig -> (Maybe String) -> (Maybe Integer) -> (Maybe String) -> IO ()
+-- tweet only if we already have a last ID, i.e. not in the first iteration
+-- maybeTweetReply (Just _) config (Just screenName) (Just tweetId) (Just text) = do
+maybeTweetReply _ config (Just screenName) (Just tweetId) (Just text) = do
+	let replyText = "@" ++ screenName ++ " " ++ text
+	let token = tokenFromConfig config
+	response <- runOAuthM token $ do
+		loadToken config
+		signRq2 HMACSHA1 Nothing tweetUrl { method = POST,
+		                                    reqHeaders = fromList [("Content-Type", "application/x-www-form-urlencoded")],
+                                            reqPayload = urlEncode [("status", replyText), ("in_reply_to_status_id", show tweetId)]
+                                          } >>= serviceRequest CurlClient
+	putStrLn $ show response
+	return ()
+maybeTweetReply _ _ _ _ _ = do
+	return ()
+
+urlEncode :: [(String,String)] -> L.ByteString
+urlEncode = L.pack . init . concatMap (\(k,v) -> PE.encode k ++ "=" ++ PE.encode v ++ "&")
 
 extractLastId :: String -> Maybe Integer
 extractLastId json =
-	maybeDecodeJSONList json >>= listToMaybe >>= extractEntry "id" >>= extractNumFromEntry
+	maybeDecodeJSONList json >>= listToMaybe >>= (extractEntry "id" :: JSObject JSValue -> Maybe JSValue) >>= extractNumFromEntry
 	where
 		extractNumFromEntry (JSRational False r) = Just $ numerator r
 		extractNumFromEntry _                    = Nothing
 
-maybeDecodeJSONList :: String -> Maybe [JSObject JSValue]
-maybeDecodeJSONList = okToMaybe . decode
+extractMention :: String -> Maybe String
+extractMention json =
+	maybeDecodeJSONList json >>= listToMaybe >>= (extractEntry "text" :: JSObject JSValue -> Maybe JSString) >>= Just . unwords . drop 1 . words . fromJSString
 
-okToMaybe :: Result a -> Maybe a
-okToMaybe (Ok j) = Just j
-okToMaybe _      = Nothing
+-- nested JSON
+extractScreenName :: String -> Maybe String
+extractScreenName json =
+	maybeDecodeJSONList json >>= listToMaybe >>= (extractEntry "user" :: JSObject JSValue -> Maybe (JSObject JSValue)) >>= (extractEntry "screen_name" :: JSObject JSValue -> Maybe JSString) >>= Just . fromJSString
+
+maybeDecodeJSONList :: String -> Maybe [JSObject JSValue]
+maybeDecodeJSONList = resultToMaybe . decode
+
+resultToMaybe :: Result a -> Maybe a
+resultToMaybe (Ok j) = Just j
+resultToMaybe _      = Nothing
 
 -- TODO: is there a way to write this point-free?
-extractEntry :: String -> JSObject JSValue -> Maybe JSValue
-extractEntry key obj = okToMaybe $ valFromObj key obj
+extractEntry key obj = resultToMaybe $ valFromObj key obj
 
 parseArgs :: [(String)] -> Maybe (String, String, String, String)
 parseArgs args =
@@ -105,4 +169,9 @@ getMentions config lastId = do
 		loadToken config
 		signRq2 HMACSHA1 Nothing (mentionsUrl sinceId) >>= serviceRequest CurlClient
 	return $ L.unpack $ rspPayload response
+
+nothingOr :: Maybe a -> Maybe a -> Maybe a
+nothingOr (Just a) _        = Just a
+nothingOr Nothing  (Just b) = Just b
+nothingOr Nothing Nothing   = Nothing
 
